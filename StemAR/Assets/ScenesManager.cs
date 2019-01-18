ffbec33853d93191a7ee7cc58cba25bd798dfdf1
jingorho <jingorho@gmail.com>
2019-01-18 17:01:02 -0500

    Made Scene Transition

---
+++ StemAR/Assets/ScenesManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class ScenesManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SwitchScene()
+    {
+        switch (SceneManager.GetActiveScene().name)
+        {
+            case "TitleScene":
+                    SceneManager.LoadScene("ChooseLessonScene");
+                    break;
+            case "ChooseLessonScene":
+                    SceneManager.LoadScene("FindingTargetScene");
+                    break;
+            case "FindingTargetScene":
+                    SceneManager.LoadScene("StartScene");
+                    break;
+            case "StartScene":
+                    SceneManager.LoadScene("GameScene_temp");
+                    break;
+            case "GameScene_temp":
+                SceneManager.LoadScene("EndScene");
+                break;
+            case "EndScene":
+                SceneManager.LoadScene("TitleScene");
+                break;
+
+        }
+    }//SwitchScene()
+
+
+    public void QuitGame()
+    {
+        if (SceneManager.GetActiveScene().name == "EndScene") { 
+            print("Quitting Application");
+            Application.Quit();
+        }
+    }//QuitGame()
+
+    public void BackToFindingTarget()
+    {
+        if (SceneManager.GetActiveScene().name == "StartScene")
+        {
+            SceneManager.LoadScene("FindingTargetScene");
+        }
+    }//BackToFindingTarget()
+
+}
